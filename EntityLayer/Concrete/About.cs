443a883f69f5e684d0c275cea342f7f907bfdf68
busragulec <83514274+busragulec@users.noreply.github.com>
2022-07-26 15:10:07 +0300

    26.07.2022 Güncelleme (49)

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -14,8 +14,8 @@ namespace EntityLayer.Concrete
         public string AboutDetails1{ get; set; }
         public string AboutDetails2 { get; set; }
         public string AboutImage1 { get; set; }
-        public int AboutImage2 { get; set; }
-        public int AboutMApLocation { get; set; }
+        public string AboutImage2 { get; set; }
+        public string AboutMapLocation { get; set; }
         public bool AboutStatus { get; set; }
     }
 }
